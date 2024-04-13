0df659b3e25fd4014b11346557101c87226b3f01
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-13 16:06:31 -0700

    Create definitions and test for singlylinkedlist

---
+++ singlylinkedlist/singlylinkedlist.go
@@ -0,0 +1,31 @@
+package singlylinkedlist
+
+type SinglyLinkedList[T comparable] struct {
+	length int
+}
+
+func New[T comparable]() SinglyLinkedList[T] {
+	return SinglyLinkedList[T]{} // TODO: Remove this
+}
+
+func (dll SinglyLinkedList[T]) Length() int {
+	return 0
+}
+
+func (dll *SinglyLinkedList[T]) Prepend(item T) {}
+
+func (dll *SinglyLinkedList[T]) InsertAt(item T, idx int) {}
+
+func (dll *SinglyLinkedList[T]) Append(item T) {}
+
+func (dll *SinglyLinkedList[T]) Remove(item T) (value T, ok bool) {
+	return
+}
+
+func (dll *SinglyLinkedList[T]) Get(idx int) (value T, ok bool) {
+	return
+}
+
+func (dll *SinglyLinkedList[T]) RemoveAt(idx int) (value T, ok bool) {
+	return
+}
