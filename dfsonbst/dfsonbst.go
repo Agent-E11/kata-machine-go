c51f7f9651d01cf7179077557e32581f025c339a
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-12 22:24:30 -0700

    Create definition and test for dfsonbst

---
+++ dfsonbst/dfsonbst.go
@@ -0,0 +1,9 @@
+package dfsonbst
+
+import (
+	"github.com/agent-e11/kata-machine-go/tree"
+)
+
+func DFS(head tree.BinaryNode[int], needle int) bool {
+	return false
+}
---
+++ dfsonbst/dfsonbst_test.go
@@ -0,0 +1,20 @@
+package dfsonbst_test
+
+import (
+	"testing"
+
+	"github.com/agent-e11/kata-machine-go/tree"
+	"github.com/agent-e11/kata-machine-go/dfsonbst"
+)
+
+func TestDFS(t *testing.T) {
+	if !dfsonbst.DFS(tree.Tree1, 45) {
+		t.Fatal("expected to find 45 in tree.Tree1")
+	}
+	if !dfsonbst.DFS(tree.Tree1, 7) {
+		t.Fatal("expected to find 7 in tree.Tree1")
+	}
+	if dfsonbst.DFS(tree.Tree1, 69) {
+		t.Fatal("expected to find 69 in tree.Tree1")
+	}
+}
