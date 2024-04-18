89d182e78c8ab31963b610683363cc23cea80e75
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-18 12:45:44 -0700

    Change name of SinglyLinkedList in methods

--- src/singlylinkedlist/singlylinkedlist.go
+++ src/singlylinkedlist/singlylinkedlist.go
@@ -11,24 +11,24 @@ func New[T comparable]() SinglyLinkedList[T] {
 	return SinglyLinkedList[T]{}
 }
 
-func (dll SinglyLinkedList[T]) Length() int {
+func (sll SinglyLinkedList[T]) Length() int {
 	return 0
 }
 
-func (dll *SinglyLinkedList[T]) Prepend(item T) {}
+func (sll *SinglyLinkedList[T]) Prepend(item T) {}
 
-func (dll *SinglyLinkedList[T]) InsertAt(item T, idx int) {}
+func (sll *SinglyLinkedList[T]) InsertAt(item T, idx int) {}
 
-func (dll *SinglyLinkedList[T]) Append(item T) {}
+func (sll *SinglyLinkedList[T]) Append(item T) {}
 
-func (dll *SinglyLinkedList[T]) Remove(item T) (value T, ok bool) {
+func (sll *SinglyLinkedList[T]) Remove(item T) (value T, ok bool) {
 	return
 }
 
-func (dll *SinglyLinkedList[T]) Get(idx int) (value T, ok bool) {
+func (sll *SinglyLinkedList[T]) Get(idx int) (value T, ok bool) {
 	return
 }
 
-func (dll *SinglyLinkedList[T]) RemoveAt(idx int) (value T, ok bool) {
+func (sll *SinglyLinkedList[T]) RemoveAt(idx int) (value T, ok bool) {
 	return
 }
