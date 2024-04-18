286390299fb93259f81cd29899d5584b8ea323f6
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-18 12:22:39 -0700

    Fix package import paths when generating katas

--- scripts/generate.py
+++ scripts/generate.py
@@ -6,6 +6,7 @@ Generate one day of katas
 
 import shutil
 from os import path
+import glob
 
 # Check if there is a src directory
 if not path.exists("src"):
@@ -23,3 +24,20 @@ print(f"creating katas in: {day_dir}")
 
 # Copy all the files in src to the new day directory
 shutil.copytree("src", day_dir)
+
+for file in glob.glob(f"{day_dir}/**/*.go"):
+
+    if path.isdir(file):
+        continue
+
+    with open(file, "r") as f:
+        content = f.read()
+
+    content = content.replace(
+        "/kata-machine-go/src/",
+        f"/kata-machine-go/{day_dir}/"
+    )
+
+    with open(file, "w") as f:
+        f.write(content)
+
